ca246d86607f7a6fae48cd0f2fbf8eda48951856
jackHay22 <jjphay@gmail.com>
2020-10-22 15:19:23 -0400

    temperature and humidity

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -3,8 +3,13 @@
  */
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <dht.h>
 
 #define AMAX 1023
+#define ANALOG0 A0
+#define DIGITAL7 7
+
+dht DHT;
 
 void setup() {
   WiFi.begin("ssid","pswd");
@@ -17,17 +22,40 @@ void setup() {
 void loop() {
 
   /*
-   * Get the reading from the sensor
+   * Get the soil moisture reading from the sensor
    */
-  double value = AMAX - (double) analogRead(A0);
+  double value = AMAX - (double) analogRead(ANALOG0);
 
   HTTPClient http;
 
   http.begin("http://10.0.0.149/api/v1/push");
   http.addHeader("Content-Type","application/json");
 
-  int status = http.POST("{\"name\":\"lemon_tree\",\"family\":\"soil_moisture\",\"value\": " + String(value) + "}");
+  http.POST("{\"name\":\"lemon_tree\",\"family\":\"soil_moisture\",\"value\": " + String(value) + "}");
 
-  //delay for 10 seconds
-  delay(10000);
+  /*
+   * Get the temperature and humidity readings
+   */
+  DHT.read11(DHT11_PIN);
+
+  /*
+   * Post the temperature
+   */
+
+  http.begin("http://10.0.0.149/api/v1/push");
+  http.addHeader("Content-Type","application/json");
+
+  http.POST("{\"name\":\"lemon_tree\",\"family\":\"temp\",\"value\": " + String(DHT.temperature) + "}");
+
+  /*
+   * Post the humidity
+   */
+
+  http.begin("http://10.0.0.149/api/v1/push");
+  http.addHeader("Content-Type","application/json");
+
+  http.POST("{\"name\":\"lemon_tree\",\"family\":\"humidity\",\"value\": " + String(DHT.humidity) + "}");
+
+  //delay for 60 seconds
+  delay(60000);
 }
